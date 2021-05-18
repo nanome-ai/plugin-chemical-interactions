91719a60e035d268dbb26b7b7fb1cb91be09311f
mjrosengrant <mike@rosengrant.me>
2021-05-18 19:17:43 +0000

    clean up variable names/ logic

--- arpeggio-service/app.py
+++ arpeggio-service/app.py
@@ -10,23 +10,28 @@ app = Flask(__name__)
 def index():
     input_file = request.files['input_file']
 
-    file_dir = '/var/output'
+    temp_dir = '/var/output'
     try:
-        os.mkdir(file_dir)
+        os.mkdir(temp_dir)
     except OSError:
-        shutil.rmtree(file_dir)
-        os.mkdir(file_dir)
+        shutil.rmtree(temp_dir)
+        os.mkdir(temp_dir)
 
-    temp_filepath = '{}/{}'.format(file_dir, input_file.filename)
-    input_file.save(temp_filepath)
-    import pdb; pdb.set_trace()
-    os.system('python clean_pdb.py -if {}'.format(temp_filepath))
-    cleaned_filename = '{}.clean.pdb'.format(input_file.filename.split('.')[0])
-    cleaned_file = '{}/{}'.format(file_dir, cleaned_filename)
-    # Remove everything but the cleaned file:
-    for filename in os.listdir(file_dir):
+    input_filename = input_file.filename
+    input_filepath = '{}/{}'.format(temp_dir, input_filename)
+    input_file.save(input_filepath)
+
+    os.system('python clean_pdb.py {}'.format(input_filepath))
+    cleaned_filename = '{}.clean.pdb'.format(input_filename.split('.')[0])
+    cleaned_filepath = '{}/{}'.format(temp_dir, cleaned_filename)
+
+    # Remove everything but the cleaned file, then rename cleaned file to match original input
+    for filename in os.listdir(temp_dir):
         if filename != cleaned_filename:
-            os.remove('{}/{}'.format(file_dir, filename))
+            os.remove('{}/{}'.format(temp_dir, filename))
+    os.system('mv {} {}'.format(cleaned_filepath, input_filepath))
+    cleaned_filepath = input_filepath
+    cleaned_filename = input_filename
 
     # Set up and run arpeggio command
     flags = '-v '
@@ -35,18 +40,15 @@ def index():
         for a_path in atom_paths:
             flags += '-s {} '.format(a_path)
 
-    output_flag = ''  # '-op {}'.format(file_dir)
-    command = 'python /arpeggio/arpeggio.py {} {} {}'.format(cleaned_file, flags, output_flag)
-    import pdb; pdb.set_trace()
+    command = 'python /arpeggio/arpeggio.py {} {}'.format(cleaned_filepath, flags)
 
     os.system(command)
-
-    import pdb; pdb.set_trace()
-
     # Zip output files, and send back to client
-    file_list = os.listdir(file_dir)
+    file_list = os.listdir(temp_dir)
     file_list.remove(cleaned_filename)
-    zipfile = shutil.make_archive('/tmp/{}'.format(input_file.filename), 'zip', file_dir)
+    zipfile = shutil.make_archive('/tmp/{}'.format(input_filename), 'zip', temp_dir)
+    shutil.rmtree(temp_dir)
+
     return send_file(zipfile)
 
 
