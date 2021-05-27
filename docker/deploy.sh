3847402a32cee5845dff6a07b23db334c5b0dfb5
mjrosengrant <mike@rosengrant.me>
2021-05-27 09:39:16 -0400

    Pass ARGS to plugin run command

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,5 +3,24 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-# TODO: Forward all args into plugin.
-docker-compose --env-file ../.env up
+ENV_FILE='../.env'
+
+NTS_HOST=''
+NTS_PORT=''
+while getopts 'a:p:vr' flag; do
+  case "${flag}" in
+    a) NTS_HOST="${OPTARG}" ;;
+    p) NTS_PORT="${OPTARG}" ;;
+  esac
+done
+ARGS="$*"
+
+# Create on the fly .env file to pass args into plugin container
+if [ -n "$ARGS" ];  then
+    tmpfile=$(mktemp)
+    echo "Generating .env"
+    echo 'ARGS=${ARGS}' > $tmpfile
+    ENV_FILE=$tmpfile
+fi 
+
+docker-compose --env-file $ENV_FILE up
