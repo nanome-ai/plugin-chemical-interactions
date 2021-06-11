5b7d3e6be60144e65d3f5d944400c41907bcf719
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-06-11 10:17:58 -0400

    Update run.py

--- nanome_chemical_interactions/run.py
+++ nanome_chemical_interactions/run.py
@@ -1,18 +1,21 @@
 import os
 import nanome
 import ChemicalInteractions
-
+from nanome.util.enums import Integrations
 
 # TODO: Make sure -r flag works
+
+
 def main():
     title = 'Chemical Interactions'
     description = (
         'A plugin to display various types of interatomic contacts '
         'between small and macro molecules'
     )
-    category = 'Analysis'
+    category = 'Interactions'
     advanced_settings = False
-    plugin = nanome.Plugin(title, description, category, advanced_settings)
+    plugin = nanome.Plugin(
+        title, description, category, advanced_settings, integrations=Integrations.interactions)
     plugin.set_plugin_class(ChemicalInteractions.ChemicalInteractions)
 
     host = os.environ.get('NTS_HOST')
