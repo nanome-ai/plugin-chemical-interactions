8102e15c9b6d2ea1fc03db0eb0657791b9176efd
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-06-14 15:35:26 -0400

    Setup integration hook (#10)
    * Update run.py
    * Add workaround to make sure plugin doesn't break before nanome-lib is released

--- nanome_chemical_interactions/run.py
+++ nanome_chemical_interactions/run.py
@@ -1,18 +1,29 @@
 import os
 import nanome
 import ChemicalInteractions
-
+from nanome.util.enums import Integrations
 
 # TODO: Make sure -r flag works
+
+
 def main():
     title = 'Chemical Interactions'
     description = (
         'A plugin to display various types of interatomic contacts '
         'between small and macro molecules'
     )
-    category = 'Analysis'
+    category = 'Interactions'
     advanced_settings = False
-    plugin = nanome.Plugin(title, description, category, advanced_settings)
+
+    # temporary hack until nanome-lib 1.22 release
+    try:
+        integrations = [Integrations.interactions]
+    except AttributeError:
+        integrations = []
+
+    plugin = nanome.Plugin(
+        title, description, category, advanced_settings, integrations=integrations)
+
     plugin.set_plugin_class(ChemicalInteractions.ChemicalInteractions)
 
     host = os.environ.get('NTS_HOST')
@@ -24,7 +35,6 @@ def main():
     items = list(configs.items())
     for key, value in items:
         configs.pop(key) if not value else None
-
     plugin.run(**configs)
 
 
