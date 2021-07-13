ffa3b095a52572501a2150f65487b1bb7076bc51
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-07-13 11:38:49 -0400

    Create ArgumentParser for setting up plugin (#36)
    * Set up ArgumentParser for run.py

--- chem_interactions/run.py
+++ chem_interactions/run.py
@@ -1,11 +1,34 @@
+import argparse
 import os
 import nanome
 import ChemicalInteractions
 from nanome.util.enums import Integrations
 
 
+def create_parser():
+    """Create command line parser For Plugin.
+
+    Some of these flags are passed down into the Plugin, and processed internally.
+    rtype: argsparser: args parser
+    """
+    parser = argparse.ArgumentParser(description='Parse Arguments to set up Nanome Plugin')
+    parser.add_argument('-a', '--host', help='connects to NTS at the specified IP address')
+    parser.add_argument('-p', '--port', help='connects to NTS at the specified port')
+    parser.add_argument('-r', '--auto-reload', action='store_true', help='Restart plugin automatically if a .py or .json file in current directory changes')
+    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose mode, to display Logs.debug')
+    parser.add_argument('-n', '--name', default='', help='Name to display for this plugin in Nanome')
+    parser.add_argument('-k', '--keyfile', help='Specifies a key file or key string to use to connect to NTS')
+    parser.add_argument('-i', '--ignore', help='To use with auto-reload. All paths matching this pattern will be ignored, use commas to specify several. Supports */?/[seq]/[!seq]')
+    return parser
+
+
 def main():
-    title = 'Chemical Interactions'
+    parser = create_parser()
+    args = parser.parse_args()
+
+    default_title = 'Chemical Interactions'
+    title = args.name or default_title
+
     description = (
         'A plugin to display various types of interatomic contacts '
         'between small and macro molecules'
@@ -19,8 +42,10 @@ def main():
 
     plugin.set_plugin_class(ChemicalInteractions.ChemicalInteractions)
 
-    host = os.environ.get('NTS_HOST')
-    port = os.environ.get('NTS_PORT') or 0
+    # CLI Args take priority over environment variables for NTS settnigs
+    host = args.host or os.environ.get('NTS_HOST')
+    port = args.port or os.environ.get('NTS_PORT') or 0
+
     configs = {
         'host': host,
         'port': int(port) if port else None
