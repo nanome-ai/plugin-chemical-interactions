bbdbc170055c925c4a581462a8bc28623820800e
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2021-07-13 11:07:19 -0400

    Set up ArgumentParser for run.py

--- chem_interactions/run.py
+++ chem_interactions/run.py
@@ -1,11 +1,31 @@
+import argparse
 import os
 import nanome
 import ChemicalInteractions
 from nanome.util.enums import Integrations
 
 
+def create_parser():
+    """Create command line parser
+    Returns:
+        argsparser: args parser
+    """
+    parser = argparse.ArgumentParser(description='Parse Flags')
+    parser.add_argument('-a', '--host', help='NTS Host.')
+    parser.add_argument('-p', '--port', help='NTS Port.')
+    parser.add_argument('-r', '--reload', action='store_true', help='Reload the plugin on code change.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose logging.')
+    parser.add_argument('-n', '--name', default='', help='Name to set for the Plugin.')
+    return parser
+
+
 def main():
-    title = 'Chemical Interactions'
+    parser = create_parser()
+    args = parser.parse_args()
+
+    default_title = 'Chemical Interactions'
+    title = args.name or default_title
+
     description = (
         'A plugin to display various types of interatomic contacts '
         'between small and macro molecules'
@@ -19,8 +39,10 @@ def main():
 
     plugin.set_plugin_class(ChemicalInteractions.ChemicalInteractions)
 
-    host = os.environ.get('NTS_HOST')
-    port = os.environ.get('NTS_PORT') or 0
+    # CLI Args take priority over environment variables for NTS settnigs
+    host = args.host or os.environ.get('NTS_HOST')
+    port = args.port or os.environ.get('NTS_PORT') or 0
+
     configs = {
         'host': host,
         'port': int(port) if port else None
