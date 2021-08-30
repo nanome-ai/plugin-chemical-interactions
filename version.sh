ed9fab1bbaf7f8df4fc0808ee3316749d192dfb2
mjrosengrant <mike@rosengrant.me>
2021-08-30 13:38:23 -0400

    updates

--- version.sh
+++ version.sh
@@ -3,18 +3,3 @@ if [ $# -eq 0 ]; then
 else
 	bump2version $1 --allow-dirty
 fi
-
-rm -rf build
-rm -rf dist
-rm -rf *.egg-info
-
-python setup.py sdist
-python setup.py bdist_wheel --universal
-
-read -rp "Upload? [yes/no] "
-
-if [[ $REPLY =~ ^yes$ ]]; then
-	twine upload dist/*
-else
-	echo "Upload canceled "
-fi
