c0f0b161ff7e4ee9a136c3c89a96a02be4e74d53
mjrosengrant <mike@rosengrant.me>
2021-10-30 11:31:30 -0400

    write deploy args to redeploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -18,7 +18,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 # Create redeploy.sh
-echo "./deploy.sh $*" > redeploy.sh
+echo "./deploy.sh $deploy_args" > redeploy.sh
 chmod +x redeploy.sh
 
 # Remove existing docker container
