6341e20d5fb748b602378f0bb7987888b2e56369
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-01-24 15:19:11 -0500

    bump nanome-lib to 0.34.0 (#66)
    * bump nanome-lib to 0.34.0
    
    * remove deprecated function

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,6 @@ chem_interactions_dir = f'{os.getcwd()}/chem_interactions/'
 sys.path.append(chem_interactions_dir)
 
 test_directory = 'tests'
-Logs._set_verbose(True)
 suite = unittest.TestLoader().discover(test_directory)
 
 output = unittest.TextTestRunner(verbosity=1).run(suite)
