a57bcd6a149e62222ba7bdca7da4d56748fd119d
Mike Rosengrant <mike.rosengrant@nanome.ai>
2023-10-18 12:12:45 -0400

    remove save_calculations btn from menu

--- plugin/menus.py
+++ plugin/menus.py
@@ -62,8 +62,6 @@ class ChemInteractionsMenu():
         self.btn_toggle_interactions = self._menu.root.find_node('Toggle Display').get_content()
         self.btn_toggle_interactions.register_pressed_callback(self.toggle_all_interactions)
 
-        self.ln_btn_save_interactions = self._menu.root.find_node('ln_btn_save_interactions')
-
     @async_callback
     async def render(self, complexes=None, default_values=False):
         complexes = complexes or []
